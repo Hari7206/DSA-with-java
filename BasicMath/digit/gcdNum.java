9097cba5f09d552361f646b9fc990bc37a633c61
Hari7206 <harithapa4654@gmail.com>
2025-05-06 18:24:30 +0530

    other method of doing the gcd numbers

--- BasicMath/digit/gcdNum.java
+++ BasicMath/digit/gcdNum.java
@@ -3,6 +3,7 @@ package BasicMath.digit;
 public class gcdNum {
     public static void main(String[] args) {
         gcds(12, 9);
+        othergcds(20,40);
     }
     public static void gcds(int n1 , int  n2 ) {
         int great = 1;   // it is as the gcd no of our code 
@@ -13,5 +14,15 @@ public class gcdNum {
         }
         System.out.println("GCD of " + n1 + " and " + n2 + " is: " + great);
     }
+    // here we will see the other method of doing the gcd 
+    public static void othergcds(int n1 , int  n2 ) {
+        for (int i = Math.min(n1 , n2); i >=1; i--) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                System.out.println("the gcd is "+i); // the i is the gcd here
+               
+            }
+        }
+    }
+
 }
 // this is how we can find the gcd in the java 
