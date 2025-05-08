7dc06d39036093c59d5f03c741571008623e500e
Hari7206 <harithapa4654@gmail.com>
2025-05-08 22:00:50 +0530

    reverse of arr

---
+++ Recursion/paramfunction/reverse.java
@@ -0,0 +1,22 @@
+package Recursion.paramfunction;
+// reverse the array using the recursion
+public class reverse {
+    public static void main(String[] args) {
+        int[] arr = { 4 , 5  , 6 , 3 , 2};
+        reverarr(arr,0, arr.length-1);
+        for (Object val : arr) {
+            System.out.println(val);
+        }
+    }
+    public static void reverarr(int arr[],int l , int r ) {
+        if (l>=r) {
+            
+        }
+        else {
+           int temp = arr[l];
+           arr[l] = arr[r];
+           arr[r] = temp;
+            reverarr(arr,l+1, r-1);
+        }
+    }
+}
