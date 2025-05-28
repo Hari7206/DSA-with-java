4bb8fb4300d056b0a07e4847b5a2bb7ca498669c
Hari7206 <harithapa4654@gmail.com>
2025-05-29 00:47:59 +0530

    check if the shorted

---
+++ Array/arrayproblem/shorted.java
@@ -0,0 +1,21 @@
+package Array.arrayproblem;
+
+
+public class shorted {
+    public static void main(String[] args) {
+         int arr[] = {90 ,89, 67 , 45 , 3 , 1};
+         boolean shorted = true;
+          for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i+1] > arr[i] ) {
+             shorted = false;
+             break;
+            }  
+        }
+        if (shorted == true) {
+            System.out.println("its an shorted array");
+        }
+        else{
+            System.out.println("its an unshorted arrays");
+        }
+    }
+}
