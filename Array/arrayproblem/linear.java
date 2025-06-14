f15ef4f5106424f2d27c9c29732b300f2b09b05c
Hari7206 <harithapa4654@gmail.com>
2025-06-14 23:18:07 +0530

    linear search

---
+++ Array/arrayproblem/linear.java
@@ -0,0 +1,13 @@
+package Array.arrayproblem;
+
+public class linear {
+    public static void main(String[] args) {
+        int arr[] = {45 , 67 , 84 , 90 ,  23 , 5 , 78 };
+        int number = 23;
+        for (int i = 0; i < arr.length; i++) {
+           if (arr[i] == number) {
+            System.out.println(arr[i] +" is found at index " + i);
+           }
+        }
+    }
+}
