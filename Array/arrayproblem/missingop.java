a14401560e952b980ae8195dca84972efd906ce0
Hari7206 <harithapa4654@gmail.com>
2025-06-16 23:31:28 +0530

    optimal version of the array number find

---
+++ Array/arrayproblem/missingop.java
@@ -0,0 +1,17 @@
+package Array.arrayproblem;
+// this is the version of the optimal version
+public class missingop {
+    public static void main(String[] args) {
+        int arr[] = {1 , 2 , 4 , 5};
+        int n =5;
+         int[] hash = new int[n + 1];
+          for (int i = 0; i < n-1; i++) {
+            hash[arr[i]] = 1;
+          }
+          for (int j = 1; j < n; j++) {
+            if (hash[j] == 0) {
+               System.out.println("missing number is " + j);
+            }
+          }
+    }
+}
