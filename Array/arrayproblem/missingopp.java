050f6f330a4a105aac4b04c9735cf572b56ac98e
Hari7206 <harithapa4654@gmail.com>
2025-06-17 23:40:45 +0530

    new

--- Array/arrayproblem/missingopp.java
+++ Array/arrayproblem/missingopp.java
@@ -4,6 +4,7 @@ public class missingopp {
     public static void main(String[] args) {
         int arr[] = {1 , 2 , 3 , 5};
         int n = 5;
+        //
         int sum = n * (n+1) / 2;
         int sumTwo = 0;
         for (int i = 0; i < arr.length; i++) {
