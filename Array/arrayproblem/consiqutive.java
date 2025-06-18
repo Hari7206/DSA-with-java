345d81196c9d530c5728c28524bfb49de5d5016b
Hari7206 <harithapa4654@gmail.com>
2025-06-18 12:31:51 +0530

    assign number

--- Array/arrayproblem/consiqutive.java
+++ Array/arrayproblem/consiqutive.java
@@ -2,12 +2,14 @@ package Array.arrayproblem;
 // find max consiqutive 1 in the array 
 public class consiqutive {
     public static void main(String[] args) {
+        // here assign 1
         int arr[] = {1 , 1 , 1 , 1 , 1, 0 , 0 , 1 , 1 , 1  , 0 , 0  , 1 , 1 , 1 , 1 , 0 };
         int count = 0;
         int max = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
                 count++;
+                // here when ever our count is max the max we will assign it the value 
           if (count > max) {
               max = count;
           }
