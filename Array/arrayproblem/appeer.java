36762fdf7fbe46cc33354194337b4c0e9344b055
Hari7206 <harithapa4654@gmail.com>
2025-06-21 00:47:27 +0530

    comment

--- Array/arrayproblem/appeer.java
+++ Array/arrayproblem/appeer.java
@@ -2,10 +2,14 @@ package Array.arrayproblem;
 public class appeer {
     // so here we willl use the brute force for the unique number 
     public static void main(String[] args) {
+        // here we will assign the unqiue number
         int arr[] = {1 , 1 , 2 , 2 , 3 , 4 , 4 , 5 , 5};
+
+        // our first itration start here
         for (int i = 0; i < arr.length; i++) {
             int count = 0;
             int num = arr[i];
+            // here for the checking the itration
             for (int j = 0; j < arr.length; j++) {
                 if (num == arr[j]) {
                     count++;
