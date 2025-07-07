cdb1079ea57e4b3b6bcaf5ec2a50b79dd90dc0ef
Hari7206 <harithapa4654@gmail.com>
2025-07-07 22:22:34 +0530

    largest array

--- arraypractice/easy/largest.java
+++ arraypractice/easy/largest.java
@@ -1,21 +1,38 @@
+// package arraypractice.easy;
+// // so first we will see the brute force of this 
+// public class largest {
+//     public static void main(String[] args) {
+//         int arr[] = {12 , 45 , 23 , 56 , 67  ,  87 , 21};
+//         int n = arr.length-1;
+//         for (int j = 0; j< arr.length; j++) {
+//             for (int i = j+1; i < arr.length; i++) {
+//                 if (arr[j] > arr[i]) {
+//                     int temp = arr[j];
+//                     arr[j] = arr[i];
+//                     arr[i] = temp;
+//                 }
+//             }
+//         }
+//         for (int i = 0; i < arr.length; i++) {
+//             System.out.println(arr[i]); 
+//         }
+//         System.out.println("the largest is "+ arr[arr.length - 1 ]);
+//     }
+// }
+
+
+// so here we will start the optimal version of the largest 
 package arraypractice.easy;
-// so first we will see the brute force of this 
+
 public class largest {
-    public static void main(String[] args) {
-        int arr[] = {12 , 45 , 23 , 56 , 67  ,  87 , 21};
-        int n = arr.length-1;
-        for (int j = 0; j< arr.length; j++) {
-            for (int i = j+1; i < arr.length; i++) {
-                if (arr[j] > arr[i]) {
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                }
-            }
+public static void main(String[] args) {
+     int arr[] = {12 , 45 , 23 , 56 , 67  ,  87 , 21 , 110};
+     int largest = 0;
+     for (int i = 0; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]); 
-        }
-        System.out.println("the largest is "+ arr[arr.length - 1 ]);
-    }
+     }
+     System.out.println("largrest is " + largest);
+}
 }
