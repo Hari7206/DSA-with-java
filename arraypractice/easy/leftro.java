96c1afc29fc051cb2ab51f84e7bbd6dcf7787543
Hari7206 <harithapa4654@gmail.com>
2025-07-09 08:33:45 +0530

    comments

--- arraypractice/easy/leftro.java
+++ arraypractice/easy/leftro.java
@@ -15,34 +15,36 @@
 //     }
 // }
 
-package arraypractice.easy;
+// package arraypractice.easy;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
-// so now here we do the left rotete by d place as the brute force approch
-public class leftro {
-    public static void main(String[] args) {
-        List<Integer> temp = new ArrayList<>();
+// // so now here we do the left rotete by d place as the brute force approch
+// public class leftro {
+//     public static void main(String[] args) {
+//         List<Integer> temp = new ArrayList<>();
       
 
-        int arr[] = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 };
-        // so left rotate it by 3 place 
-        int d = 3;
-        int n = arr.length;
+//         int arr[] = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 };
+//         // so left rotate it by 3 place 
+//         int d = 3;
+//         int n = arr.length;
         
-        for (int i = 0; i < d; i++) {
-            temp.add(arr[i]);
-        }
-       for (int i = d; i < n; i++) {
-        arr[i - d] = arr[i];
-       }
+//         for (int i = 0; i < d; i++) {
+//             temp.add(arr[i]);
+//         }
+//        for (int i = d; i < n; i++) {
+//         arr[i - d] = arr[i];
+//        }
 
-       for (int i = n - d; i < n; i++) {
-        arr[i] = temp.get(i-(n-d));
-       }
-for (int elem : arr) {
-    System.out.println(elem);
-}
-    }
-}
+//        for (int i = n - d; i < n; i++) {
+//         arr[i] = temp.get(i-(n-d));
+//        }
+// for (int elem : arr) {
+//     System.out.println(elem);
+// }
+//     }
+// }
+
+
