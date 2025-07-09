2d1ba64dccde0e0fbffc02c84d116793299c10b5
Hari7206 <harithapa4654@gmail.com>
2025-07-09 09:33:55 +0530

    optimal version

--- arraypractice/easy/leftro.java
+++ arraypractice/easy/leftro.java
@@ -47,4 +47,36 @@
 //     }
 // }
 
+package arraypractice.easy;
+
+
+
+// // so now here we do the left rotete by d place as the optimal  force approch
+public class leftro {
+    public static void main(String[] args) {
+        int arr[] = {1 ,2 , 3 , 4  , 5 , 6 , 7};
+        int d = 3;
+        int n = arr.length;
+        reverse(arr, 0, d-1);
+        reverse(arr, d, n-1);
+        reverse(arr, 0, n-1);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        
+    }
+    public static void reverse(int arr[] , int  first , int d) {
+
+        while (first <= d) 
+          {
+            int temp = arr[first];
+            arr[first] = arr[d];
+            arr[d] = temp;
+            first++;
+            d--;
+        }
+    }
+}
 
