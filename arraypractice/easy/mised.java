51c235385a155237daead223d33a37e816142cf5
Hari7206 <harithapa4654@gmail.com>
2025-07-11 09:20:40 +0530

    missing number

--- arraypractice/easy/mised.java
+++ arraypractice/easy/mised.java
@@ -20,22 +20,46 @@
 //     }
 // }
 
+// package arraypractice.easy;
+// // so here we will see the brute force approch of the missing array number 
+
+// public class mised {
+// public static void main(String[] args) {
+//     int arr[] = { 1 , 2 , 3  , 4  , 6};
+//     int n = 6;
+//     int hash[] = new int[n+1];
+
+//     for (int i = 0; i < arr.length; i++) {
+//             hash[arr[i]] = 1;
+//     }
+//     for (int j = 1; j < hash.length; j++) {
+//         if (hash[j] == 0) {
+//             System.out.println("the missing number is " + j);
+//         }
+//     }
+// }
+// }
+
 package arraypractice.easy;
 // so here we will see the brute force approch of the missing array number 
 
 public class mised {
-public static void main(String[] args) {
-    int arr[] = { 1 , 2 , 3  , 4  , 6};
-    int n = 6;
-    int hash[] = new int[n+1];
 
-    for (int i = 0; i < arr.length; i++) {
-            hash[arr[i]] = 1;
+    public static void main(String[] args) {
+         int arr[] = { 1 , 2 , 3  , 4  , 6};
+
+         int n = 6;
+         int sumOne = n*(n+1)/2;
+         System.out.println(sumOne);
+        int sumTwo = 0;
+         for (int  i = 0; i < arr.length; i++) {
+            sumTwo = sumTwo + arr[i];
+         }
+         System.out.println(sumTwo);
+         int results = sumOne - sumTwo;
+         System.out.println("the missing number is " + results);
+    
+
     }
-    for (int j = 1; j < hash.length; j++) {
-        if (hash[j] == 0) {
-            System.out.println("the missing number is " + j);
-        }
-    }
-}
+
 }
