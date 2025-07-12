4170ae9db7e851503f3f2662cb43c4390f30a92b
Hari7206 <harithapa4654@gmail.com>
2025-07-12 08:07:47 +0530

    xor optimal

--- arraypractice/easy/mised.java
+++ arraypractice/easy/mised.java
@@ -40,26 +40,54 @@
 // }
 // }
 
+// package arraypractice.easy;
+// // so here we will see the optimal approch of the missing array number 
+
+// public class mised {
+
+//     public static void main(String[] args) {
+//          int arr[] = { 1 , 2 , 3  , 4  , 6};
+
+//          int n = 6;
+//          int sumOne = n*(n+1)/2;
+//          System.out.println(sumOne);
+//         int sumTwo = 0;
+//          for (int  i = 0; i < arr.length; i++) {
+//             sumTwo = sumTwo + arr[i];
+//          }
+//          System.out.println(sumTwo);
+//          int results = sumOne - sumTwo;
+//          System.out.println("the missing number is " + results);
+    
+
+//     }
+
+// }
+
 package arraypractice.easy;
-// so here we will see the optimal approch of the missing array number 
+// // so here we will see the optimal approch of the missing array number 
+// If bits are the same → result is 0
+// If bits are different → result is 1     this is for the purpose of remeber the xor 
 
 public class mised {
 
     public static void main(String[] args) {
-         int arr[] = { 1 , 2 , 3  , 4  , 6};
+        int arr[] = {1 ,2 , 3 , 5 , 6};
+        int n = arr.length + 1;
+        int xor1 = 0;
+        int xor2 = 0;
+        int result = 0;
+        for (int i = 0; i <= n; i++) {
+            xor1 = xor1 ^ i;
+        }
 
-         int n = 6;
-         int sumOne = n*(n+1)/2;
-         System.out.println(sumOne);
-        int sumTwo = 0;
-         for (int  i = 0; i < arr.length; i++) {
-            sumTwo = sumTwo + arr[i];
-         }
-         System.out.println(sumTwo);
-         int results = sumOne - sumTwo;
-         System.out.println("the missing number is " + results);
-    
+            for (int j = 0; j < arr.length; j++) {
+            xor2 = xor2^arr[j];
+            }
 
+             result = xor1 ^ xor2;
+        System.out.println(result);
+
+        
     }
-
 }
