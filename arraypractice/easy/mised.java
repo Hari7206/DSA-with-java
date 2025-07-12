bfe62f5249bb07c8b401f739e6113e2aab235782
Hari7206 <harithapa4654@gmail.com>
2025-07-12 08:12:02 +0530

    xor more optimal version

--- arraypractice/easy/mised.java
+++ arraypractice/easy/mised.java
@@ -64,6 +64,34 @@
 
 // }
 
+// package arraypractice.easy;
+// // // so here we will see the optimal approch of the missing array number 
+// // If bits are the same → result is 0
+// // If bits are different → result is 1     this is for the purpose of remeber the xor 
+
+// public class mised {
+
+//     public static void main(String[] args) {
+//         int arr[] = {1 ,2 , 3 , 5 , 6};
+//         int n = arr.length + 1;
+//         int xor1 = 0;
+//         int xor2 = 0;
+//         int result = 0;
+//         for (int i = 0; i <= n; i++) {
+//             xor1 = xor1 ^ i;
+//         }
+
+//             for (int j = 0; j < arr.length; j++) {
+//             xor2 = xor2^arr[j];
+//             }
+
+//              result = xor1 ^ xor2;
+//         System.out.println(result);
+
+        
+//     }
+// }
+
 package arraypractice.easy;
 // // so here we will see the optimal approch of the missing array number 
 // If bits are the same → result is 0
@@ -77,14 +105,11 @@ public class mised {
         int xor1 = 0;
         int xor2 = 0;
         int result = 0;
-        for (int i = 0; i <= n; i++) {
-            xor1 = xor1 ^ i;
-        }
-
             for (int j = 0; j < arr.length; j++) {
             xor2 = xor2^arr[j];
+            xor1 = xor1 ^ (j+1);
             }
-
+            xor1 = xor1 ^ n;
              result = xor1 ^ xor2;
         System.out.println(result);
 
