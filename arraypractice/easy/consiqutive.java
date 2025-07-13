e171a97c81e9055b72b4b97b57acb4620c4a4ca6
Hari7206 <harithapa4654@gmail.com>
2025-07-13 20:42:28 +0530

    comment

--- arraypractice/easy/consiqutive.java
+++ arraypractice/easy/consiqutive.java
@@ -1,11 +1,12 @@
 package arraypractice.easy;
-// so here we will check the consiqutiveness of the 1 
+// so here we will check the consiqutiveness offs the 1 
 public class consiqutive {
     public static void main(String[] args) {
         int arr[] = {1 , 1 , 0 , 1 , 1 , 1 , 0 , 0 , 1 };
         int count = 0;
         int max = -1;
         for (int i = 0; i < arr.length; i++) {
+            //  
             if (arr[i] == 1 ) {
                 count++;       
                 if ( count > max) {
