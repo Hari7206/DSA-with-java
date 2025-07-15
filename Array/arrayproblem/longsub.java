8dccd2221e2738fb7e14945523d29397d8377865
Hari7206 <harithapa4654@gmail.com>
2025-07-15 08:45:10 +0530

    brute of longest sum array

--- Array/arrayproblem/longsub.java
+++ Array/arrayproblem/longsub.java
@@ -1,22 +1,46 @@
+// package Array.arrayproblem;
+// // so here we will do the longest sum of subarray
+
+// public class longsub {
+//     public static void main(String[] args) {
+//         int arr[] = {1 , 2 , 5 , 6 , 1 , 1 , 4 , 3 , 6};
+//         int length = 0;
+//         int target = 6;
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j = i; j < arr.length; j++) {
+//                 int submission = 0;
+//                 for (int k = i; k <= j; k++ ) {
+//                     submission = submission + arr[k];
+//                 }
+//                 if (submission == target) {
+//                     length = Math.max(length, j-i+1);
+//                 }
+//             }
+//         }
+//         System.out.println(length);
+//     }
+// }
+
 package Array.arrayproblem;
-// so here we will do the longest sum of subarray
+// so here we will do the longest sum of subarray another brute force approch 
 
 public class longsub {
-    public static void main(String[] args) {
-        int arr[] = {1 , 2 , 5 , 6 , 1 , 1 , 4 , 3 , 6};
-        int length = 0;
-        int target = 6;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
-                int submission = 0;
-                for (int k = i; k <= j; k++ ) {
-                    submission = submission + arr[k];
-                }
-                if (submission == target) {
-                    length = Math.max(length, j-i+1);
-                }
+
+public static void main(String[] args) {
+    int arr[] = {1 , 2 , 5 , 6 , 1 , 1 , 4 , 3 , 6};
+    int target = 12;
+    int length = 0;
+    for (int i = 0; i < arr.length; i++) {
+        int submission = 0;
+        for (int j = i; j < arr.length; j++) {
+            submission = submission + arr[j];
+            if (submission == target) {
+                length = Math.max(length, j-i+1);
             }
         }
-        System.out.println(length);
     }
+    System.out.println(length);
+
+}
+
 }
