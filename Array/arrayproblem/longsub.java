ded20cc54d4c2012693b282a99fbbb3465a09604
Hari7206 <harithapa4654@gmail.com>
2025-07-15 08:38:36 +0530

    longest subarray

---
+++ Array/arrayproblem/longsub.java
@@ -0,0 +1,22 @@
+package Array.arrayproblem;
+// so here we will do the longest sum of subarray
+
+public class longsub {
+    public static void main(String[] args) {
+        int arr[] = {1 , 2 , 5 , 6 , 1 , 1 , 4 , 3 , 6};
+        int length = 0;
+        int target = 6;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int submission = 0;
+                for (int k = i; k <= j; k++ ) {
+                    submission = submission + arr[k];
+                }
+                if (submission == target) {
+                    length = Math.max(length, j-i+1);
+                }
+            }
+        }
+        System.out.println(length);
+    }
+}
