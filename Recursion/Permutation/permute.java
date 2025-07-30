a706dc148a8223f838d782fcf6c98b27ad11c0ff
Hari Thapa <harithapa4654@gmail.com>
2025-07-30 09:05:17 +0530

    brute of permutation

---
+++ Recursion/Permutation/permute.java
@@ -0,0 +1,40 @@
+package Recursion.Permutation;
+
+import java.util.*; 
+
+public class permute {
+
+    static class Solution {
+        private void recurPermute(int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean[] freq) {
+            if (ds.size() == nums.length) {
+                ans.add(new ArrayList<>(ds));
+                return;
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                if (!freq[i]) {
+                    freq[i] = true;
+                    ds.add(nums[i]);
+                    recurPermute(nums, ds, ans, freq);
+                    ds.remove(ds.size() - 1);
+                    freq[i] = false;
+                }
+            }
+        }
+
+        public List<List<Integer>> permute(int[] nums) {
+            List<List<Integer>> ans = new ArrayList<>();
+            List<Integer> ds = new ArrayList<>();
+            boolean[] freq = new boolean[nums.length];
+            recurPermute(nums, ds, ans, freq);
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> result = sol.permute(nums);
+        System.out.println(result);
+    }
+}
