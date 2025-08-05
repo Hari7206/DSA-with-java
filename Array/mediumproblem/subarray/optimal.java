b8e88e4f79efe631c2555f7d2a430928ca96ad4a
Hari Thapa <harithapa4654@gmail.com>
2025-08-05 09:37:01 +0530

    here optimal version

---
+++ Array/mediumproblem/subarray/optimal.java
@@ -0,0 +1,24 @@
+package Array.mediumproblem.subarray;
+
+import java.util.HashMap;
+
+public class optimal {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> prefixSumMap = new HashMap<>();
+         int arr[] = {1 , 2 , 3 , -3 , 1  , 1 , 1  , 4 , 2 , -3};
+         int sum = 0;
+         int cout = 0;
+         int k = 3;
+
+         prefixSumMap.put(0, 1);
+
+         for (int i = 0; i < arr.length; i++) {
+            sum = sum +arr[i];
+            if (prefixSumMap.containsKey(sum - k)) {
+                cout += prefixSumMap.get(sum-k);
+            }
+             prefixSumMap.put(sum , prefixSumMap.getOrDefault(sum, 0) + 1);
+         }
+         System.out.println(cout);
+    }
+}
