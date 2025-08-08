4e7baa97406f20d05459478a76027a0523ea995e
Hari Thapa <harithapa4654@gmail.com>
2025-08-08 09:15:59 +0530

    better version

---
+++ Array/hardproblem/fourSum/better.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class better {
+    public static void main(String[] args) {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        int n = nums.length;
+
+        List<List<Integer>> result = new ArrayList<>();
+        Set<String> seen = new HashSet<>();
+
+        Arrays.sort(nums); 
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Set<Integer> hashset = new HashSet<>();
+                for (int k = j + 1; k < n; k++) {
+                    int fourth = target - nums[i] - nums[j] - nums[k];
+                    if (hashset.contains(fourth)) {
+                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[k], fourth);
+                        Collections.sort(quad); 
+                        String key = quad.toString();
+                        if (!seen.contains(key)) {
+                            result.add(quad);
+                            seen.add(key);
+                        }
+                    }
+                    hashset.add(nums[k]);
+                }
+            }
+        }
+
+        
+        for (List<Integer> quad : result) {
+            System.out.println(quad);
+        }
+    }
+}
