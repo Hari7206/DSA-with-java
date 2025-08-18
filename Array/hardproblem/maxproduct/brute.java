4d6dd18a566502425e163182d38f55dfde4c8aec
Hari Thapa <harithapa4654@gmail.com>
2025-08-18 08:01:52 +0530

    brute of max productz

---
+++ Array/hardproblem/maxproduct/brute.java
@@ -0,0 +1,24 @@
+package Array.hardproblem.maxproduct;
+
+public class brute {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, -2, 4};
+        System.out.println("Maximum product subarray: " + maxProduct(nums));
+    }
+
+    // Brute force O(n^2) approach
+    public static int maxProduct(int[] nums) {
+        int n = nums.length;
+        int maxProduct = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int product = 1;
+            for (int j = i; j < n; j++) {
+                product *= nums[j];
+                maxProduct = Math.max(maxProduct, product);
+            }
+        }
+
+        return maxProduct;
+    }
+}
--- Array/hardproblem/mergersort/optimal.java
+++ Array/hardproblem/mergersort/optimal.java
@@ -1,7 +1,37 @@
 package Array.hardproblem.mergersort;
 
+import java.util.Arrays;
+
 public class optimal {
     public static void main(String[] args) {
-        
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        int p1 = m - 1;        
+        int p2 = n - 1;      
+        int p = m + n - 1;     
+
+
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+
+      
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
+
+        System.out.println(Arrays.toString(nums1));
     }
 }
